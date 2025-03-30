d75e3c2e5dea6caed67ff0068206d6b7d8c6f679
la239239 <la239239@student.helha.be>
2025-03-30 01:33:44 +0100

    Sauvegarde des changements avant pull

--- SpongeBob/src/application/SceneSwitcher.java
+++ SpongeBob/src/application/SceneSwitcher.java
@@ -9,14 +9,19 @@ import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 
 public class SceneSwitcher {
-
     public static void switchScene(MouseEvent event, String fxmlFile) {
         try {
             Parent root = FXMLLoader.load(SceneSwitcher.class.getResource(fxmlFile));
-            
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            
             Scene scene = new Scene(root);
+            // Appliquez le CSS si nécessaire (comme dans Main.java)
+            scene.getStylesheets().add(SceneSwitcher.class.getResource("/application/application.css").toExternalForm());
+            
+            // Conserve le mode plein écran si déjà activé
+            boolean wasFullScreen = stage.isFullScreen();
             stage.setScene(scene);
+            stage.setFullScreen(wasFullScreen); // Rétablit le plein écran
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
